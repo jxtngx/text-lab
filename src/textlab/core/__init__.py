d5fd7f4a5e07ad1688012a8efe2eb769a46754a2
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-10-08 20:23:16 -0400

    update

---
+++ src/textlab/core/__init__.py
@@ -0,0 +1,2 @@
+from textlab.core.trainer import LabTrainer
+from textlab.core.module import LabModule
--- src/textlab/pipeline/__init__.py
+++ src/textlab/pipeline/__init__.py
@@ -1,1 +1,2 @@
 from pytorch_lightning.demos.transformer import WikiText2 as LabDataset  # noqa
+from textlab.pipeline.datamodule import LabDataModule
