2e48566f9d6d488c2339d63fccbf3eb3f8d86119
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-10-11 09:17:39 -0400

    update

--- src/textlab/core/module.py
+++ src/textlab/core/module.py
@@ -1,28 +1,106 @@
-import pytorch_lightning as pl
-from pytorch_lightning.demos.transformer import Transformer
+"""Demo of a simple transformer language model.
+
+Code is adapted from the PyTorch examples at
+https://github.com/pytorch/examples/blob/main/word_language_model
+
+"""
+import math
+from typing import Optional
 
 
-class LabModule(pl.LightningModule):
-    """a custom PyTorch Lightning LightningModule"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn.modules import MultiheadAttention
 
-    def __init__(self):
+from pytorch_lightning import LightningModule
+
+if hasattr(MultiheadAttention, "_reset_parameters") and not hasattr(MultiheadAttention, "reset_parameters"):
+    # See https://github.com/pytorch/pytorch/issues/107909
+    MultiheadAttention.reset_parameters = MultiheadAttention._reset_parameters
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self, vocab_size: int, ninp: int = 200, nhead: int = 2, nhid: int = 200, nlayers: int = 2, dropout: float = 0.2
+    ) -> None:
         super().__init__()
-        self.model = Transformer()
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        self.embedding = nn.Embedding(vocab_size, ninp)
+        self.transformer = nn.Transformer(
+            d_model=ninp,
+            nhead=nhead,
+            num_encoder_layers=nlayers,
+            num_decoder_layers=nlayers,
+            dim_feedforward=nhid,
+            dropout=dropout,
+            batch_first=True,
+        )
+        self.decoder = nn.Linear(ninp, vocab_size)
 
-    def forward(self, x):
-        pass
+        self.ninp = ninp
+        self.vocab_size = vocab_size
+        self.src_mask = None
 
-    def training_step(self, batch):
-        pass
+    def forward(self, inputs: Tensor, target: Tensor, mask: Optional[Tensor] = None) -> Tensor:
+        b, t = inputs.shape
 
-    def test_step(self, batch, *args):
-        pass
+        # we assume target is already shifted w.r.t. inputs
+        if mask is None:
+            mask = torch.tril(torch.ones(t, t, device=inputs.device)) == 1
+            mask = mask.float().masked_fill(mask == 0, float("-inf")).masked_fill(mask == 1, float(0.0))
 
-    def validation_step(self, batch, *args):
-        pass
+        src = self.pos_encoder(self.embedding(inputs) * math.sqrt(self.ninp))
+        target = self.pos_encoder(self.embedding(target) * math.sqrt(self.ninp))
+        output = self.transformer(src, target, tgt_mask=mask)
+        output = self.decoder(output)
+        output = F.log_softmax(output, dim=-1)
+        output = output.view(-1, self.vocab_size)
+        return output
 
-    def predict_step(self, batch, batch_idx, dataloader_idx=0):
-        pass
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim: int, dropout: float = 0.1, max_len: int = 5000) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.dim = dim
+        self.max_len = max_len
+
+        pe = self._init_pos_encoding()
+        # workaround, can't use buffer, see https://github.com/pytorch/pytorch/issues/68407
+        self.register_parameter("pe", nn.Parameter(pe, requires_grad=False))
+
+    def reset_parameters(self) -> None:
+        self.pe.copy_(self._init_pos_encoding())  # type: ignore[operator]
+
+    def forward(self, x: Tensor) -> Tensor:
+        x + self.pe[: x.size(0), :]  # type: ignore[index]
+        return self.dropout(x)
+
+    def _init_pos_encoding(self) -> Tensor:
+        pe = torch.zeros(self.max_len, self.dim)
+        position = torch.arange(0, self.max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, self.dim, 2).float() * (-math.log(10000.0) / self.dim))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        return pe
+
+
+class LabModule(LightningModule):
+    def __init__(self, vocab_size: int = 33278):
+        super().__init__()
+        self.model = Transformer(vocab_size=vocab_size)
+
+    def forward(self, inputs, target):
+        return self.model(inputs, target)
+
+    def training_step(self, batch, batch_idx):
+        inputs, target = batch
+        output = self(inputs, target)
+        loss = torch.nn.functional.nll_loss(output, target.view(-1))
+        return loss
 
     def configure_optimizers(self):
-        pass
+        return torch.optim.SGD(self.model.parameters(), lr=0.1)
--- src/textlab/pipeline/datamodule.py
+++ src/textlab/pipeline/datamodule.py
@@ -2,51 +2,50 @@ import multiprocessing
 import os
 from pathlib import Path
 
-from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data import DataLoader, random_split
 
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
+
+from textlab import config
 from textlab.pipeline import LabDataset
 
-filepath = Path(__file__)
-PROJECTPATH = os.getcwd()
 NUMWORKERS = int(multiprocessing.cpu_count() // 2)
 
 
-class LabDataModule(LightningDataModule):
+class LabDataModule(pl.LightningDataModule):
     def __init__(
         self,
-        dataset: Dataset = LabDataset,
-        data_dir: str = "data",
-        split: bool = True,
-        train_size: float = 0.8,
         num_workers: int = NUMWORKERS,
-        transforms=None,
-    ):
+        data_dir: Path = Path("./data"),
+        block_size: int = 35,
+        download: bool = True,
+        train_size: float = 0.8,
+    ) -> None:
         super().__init__()
-        self.data_dir = os.path.join(PROJECTPATH, data_dir, "cache")
-        self.dataset = dataset
-        self.split = split
-        self.train_size = train_size
+        self.data_dir = data_dir
+        self.block_size = block_size
+        self.download = download
         self.num_workers = num_workers
-        self.transforms = transforms
+        self.train_size = train_size
+        self.dataset = None
 
-    def prepare_data(self):
-        self.dataset(self.data_dir, download=True)
+    def prepare_data(self) -> None:
+        self.dataset = LabDataset(data_dir=self.data_dir, block_size=self.block_size, download=self.download)
 
-    def setup(self, stage=None):
+    def setup(self, stage: str) -> None:
         if stage == "fit" or stage is None:
-            full_dataset = self.dataset(self.data_dir, train=True, transform=self.transforms)
-            train_size = int(len(full_dataset) * self.train_size)
-            test_size = len(full_dataset) - train_size
-            self.train_data, self.val_data = random_split(full_dataset, lengths=[train_size, test_size])
+            train_size = int(len(self.dataset) * self.train_size)
+            test_size = len(self.dataset) - train_size
+            self.train_data, self.val_data = random_split(self.dataset, lengths=[train_size, test_size])
         if stage == "test" or stage is None:
-            self.test_data = self.dataset(self.data_dir, train=False, transform=self.transforms)
+            self.test_data = self.val_data
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
         return DataLoader(self.train_data, num_workers=self.num_workers)
 
-    def test_dataloader(self):
-        return DataLoader(self.test_data, num_workers=self.num_workers)
-
-    def val_dataloader(self):
+    def val_dataloader(self) -> EVAL_DATALOADERS:
         return DataLoader(self.val_data, num_workers=self.num_workers)
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_data, num_workers=self.num_workers)
